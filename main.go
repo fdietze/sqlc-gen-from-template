8f8cb9d8ec7f9637e5a48a86c0b35ceb7622b3af
asday <sara.and.zuka@gmail.com>
2025-03-02 20:42:08 +0000

    Ran `go fmt`.

--- main.go
+++ main.go
@@ -57,7 +57,7 @@ func generate(ctx context.Context, req *plugin.GenerateRequest) (*plugin.Generat
 			}
 			return option
 		},
-		"Split": strings.Split,
+		"Split":   strings.Split,
 		"ToLower": strings.ToLower,
 	}
 
